ac669624297dc16fe95910f78cd750384fdb4a1c
lesnikovskiy <lesnikovski@gmail.com>
2017-09-19 22:13:04 +0300

    JavaScript basics

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+
+    $("#username").text("Shawn Wildermuth");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.css({ backgroundColor: '#888' });
+    });
+
+    main.on("mouseleave", function () {
+        main.css({ backgroundColor: '' });
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text())
+    });
+})();
