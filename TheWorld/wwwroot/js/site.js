c3396fd7be9ac31cd5a85b56da16668181db7f10
lesnikovskiy <lesnikovski@gmail.com>
2017-09-19 22:46:55 +0300

    practical jQuery

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,20 +1,30 @@
 ﻿(function () {
 
 
-    $("#username").text("Shawn Wildermuth");
+    //$("#username").text("Shawn Wildermuth");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.css({ backgroundColor: '#888' });
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css({ backgroundColor: '#888' });
+    //});
 
-    main.on("mouseleave", function () {
-        main.css({ backgroundColor: '' });
-    });
+    //main.on("mouseleave", function () {
+    //    main.css({ backgroundColor: '' });
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text())
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text())
+    //});
+
+    var $sidebarAndWrapper = $('#sidebar,#wrapper');
+    $('#sidebarToggle').on('click', function () {
+        $sidebarAndWrapper.toggleClass('hide-sidebar');
+        if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
+            $(this).text('Show Sidebar');
+        } else {
+            $(this).text('Hide Sidebar');
+        }
     });
 })();
