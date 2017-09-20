572467b536828e616a4333a6450eb402a81e93cd
lesnikovskiy <lesnikovski@gmail.com>
2017-09-20 22:28:56 +0300

    Add IsEnviromnet check

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -22,7 +22,11 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
 
             app.UseStaticFiles();
 
